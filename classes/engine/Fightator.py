2d827fdbac83149c44073f8ed597ce982ee8fbd3
Jonas <j.bertin@maxicoffee.com>
2020-02-12 14:03:10 +0100

    begin Fight engine

--- classes/engine/Fightator.py
+++ classes/engine/Fightator.py
@@ -2,8 +2,15 @@
 import os
 import time
 from colorama import Fore, Back, Style
+from classes.char.Character import Character
 
 class Fightator():
 
-    def quickBattle():
-        return
+    def __init__(self, me):
+        self.turn = 1
+        self.monster = Character('monster')
+
+    def quickBattle(self, me):
+        self.__init__(self, me)
+        print('Monster ::::'+self.monster.name)
+        print('Player ::::'+me.name)
