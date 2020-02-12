25d596ba307337aeec0971717c586cbf8fe35d39
Jonas <j.bertin@maxicoffee.com>
2020-02-12 14:02:55 +0100

    rebuild class character __init__ funct does not return any value -> init object

--- classes/char/Character.py
+++ classes/char/Character.py
@@ -1,40 +1,31 @@
 # -*- coding: utf-8 -*-
 import json
+import random
 
 
-class Character():
+class Character:
+
     def __init__(self, charType):
         if charType == 'monster':
-            monster =self.monster(self, charType)
-            return monster
+            with open('settings/monsters.json') as f:
+                data = json.load(f)
+            monster = random.choice(data)
+            self.name = monster['name']
+            self.hp = random.randint(monster['minHp'], monster['maxHp'])
+            self.maxHp = self.hp
+            self.atk = random.randint(monster['minAtk'], monster['maxAtk'])
+            self.acr = random.randint(monster['minAcr'], monster['maxAcr'])
+            self.defc = monster['def']
+            self.xp = random.randint(monster['expMin'], monster['expMax'])
         else:
-            perso = self.character(self, charType)
-            return perso
-
-    def character(self, charType):
-        with open("settings/classes.json", "r") as f:
-            self.charClass = json.load(f)
-        this = self.charClass[charType]
-        self.id = charType
-        self.name = this['name']
-        self.hp = this['hp']
-        self.maxHp = this['hp']
-        self.atk = this['atk']
-        self.defc = this['def']
-        self.acr = this['acr']
-        self.spe = this['spe']
-        return self
-
-    def monster(self, monster):
-        with open('settings/monsters.json') as f:
-            data = json.load(f)
-        monster = random.choice(data)
-        self.name = monster['name']
-        self.hp = random.randint(monster['minHp'], monster['maxHp'])
-        self.maxHp = self.hp
-        self.atk = random.randint(monster['minAtk'], monster['maxAtk'])
-        self.acr = random.randint(monster['minAcr'], monster['maxAcr'])
-        self.defc = monster['def']
-        self.xp = random.randint(monster['expMin'], monster['expMax'])
-        return self
-        # test = self.spell(monster['spel'])
+            with open("settings/classes.json", "r") as f:
+                self.charClass = json.load(f)
+            this = self.charClass[charType]
+            self.id = charType
+            self.name = this['name']
+            self.hp = this['hp']
+            self.maxHp = this['hp']
+            self.atk = this['atk']
+            self.defc = this['def']
+            self.acr = this['acr']
+            self.spe = this['spe']
