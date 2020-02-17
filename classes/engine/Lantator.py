55834d53432f5b96c394378394a66dc9edc10416
Crash-Zeus <jonas.bertin49@gmail.com>
2020-02-17 23:57:14 +0100

    begin lantator

--- classes/engine/Lantator.py
+++ classes/engine/Lantator.py
@@ -1,8 +1,9 @@
-
+from classes.engine.Printator import Printator
 
 class Lantator:
 
     def init(self):
+        Printator.success('ini lantator')
         # choice hoster or not
         # if hoster -> choice port of lan game
         # if not hoster -> enter port of lan game
