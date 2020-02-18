08a938184209ce8ac036d5402acee13309d9ed5f
Jonas <j.bertin@maxicoffee.com>
2020-02-18 08:43:01 +0100

    Comment for lan

--- classes/engine/Lantator.py
+++ classes/engine/Lantator.py
@@ -1,9 +1,16 @@
+import socket
+import time
+import select
 from classes.engine.Printator import Printator
 
 class Lantator:
 
     def init(self):
         Printator.success('ini lantator')
+        # Printator.lanMenu(Printator)
+        # si select == host
+        # selectionner le port pour la lan et crée le serveur d'écoute?
+        # else selectionner le port sans crée de serveur d'écoute?
         # choice hoster or not
         # if hoster -> choice port of lan game
         # if not hoster -> enter port of lan game
