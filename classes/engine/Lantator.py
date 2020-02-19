f310bfed0c89a7a1ee63acd654f0934b5032852f
Jonas <j.bertin@maxicoffee.com>
2020-02-19 14:07:12 +0100

    add lantator begin server

--- classes/engine/Lantator.py
+++ classes/engine/Lantator.py
@@ -6,8 +6,22 @@ from classes.engine.Printator import Printator
 class Lantator:
 
     def init(self):
-        Printator.success('ini lantator')
-        # Printator.lanMenu(Printator)
+        menu = Printator.lanMenu(Printator)
+        if menu == 'host':
+            print('Choose port number :')
+            self.port = input('> ')
+            self.host = ''
+            self.mainConnection = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+            self.mainConnection.setsockopt(socket.SOL_SOCKET, socket.SO_REUSEADDR, 1)
+            self.mainConnection.bind(('', int(self.port)))
+            print('Hosting lan, wainting for client')
+            self.mainConnection.listen(10)
+            # no enought values to unpack ?
+            (self.client, self.host, self.port) = self.mainConnection.accept()
+        elif menu == 'join':
+            print('join')
+        elif menu == False:
+            return False
         # si select == host
         # selectionner le port pour la lan et crée le serveur d'écoute?
         # else selectionner le port sans crée de serveur d'écoute?
