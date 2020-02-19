635c3ef215aadcfa6e2013bd615bebd00b1dcbb5
Crash-Zeus <jonas.bertin49@gmail.com>
2020-02-19 20:20:41 +0100

    Add server & client socket

--- classes/engine/Lantator.py
+++ classes/engine/Lantator.py
@@ -6,20 +6,34 @@ from classes.engine.Printator import Printator
 class Lantator:
 
     def init(self):
+        self.host = '127.0.0.1'
         menu = Printator.lanMenu(Printator)
         if menu == 'host':
             print('Choose port number :')
             self.port = input('> ')
-            self.host = ''
             self.mainConnection = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
             self.mainConnection.setsockopt(socket.SOL_SOCKET, socket.SO_REUSEADDR, 1)
             self.mainConnection.bind(('', int(self.port)))
             print('Hosting lan, wainting for client')
             self.mainConnection.listen(10)
-            # no enought values to unpack ?
-            (self.client, self.host, self.port) = self.mainConnection.accept()
+            (self.connection, self.port) = self.mainConnection.accept()
+            servUp = True
+            while servUp == True:
+                try:
+                    request = self.connection.recv(2048)
+                    if request:
+                        data = request.decode()
+                        print(data)
+                except Exception as e:
+                    print(e)
         elif menu == 'join':
-            print('join')
+            print('Enter port number :')
+            self.port = input('> ')
+            self.connection = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+            self.connection.connect((self.host, int(self.port)))
+            print('Connection etablished on port {0}'.format(self.port))
+            data = input('>>> ')
+            self.connection.send(data.encode())
         elif menu == False:
             return False
         # si select == host
