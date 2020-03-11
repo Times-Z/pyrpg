56bbebc436df1ce5bc142989575a37203b47d143
Crash-Zeus <jonas.bertin49@gmail.com>
2020-03-11 21:44:46 +0100

    Real constructor

--- app/classes/engine/Apitator.py
+++ app/classes/engine/Apitator.py
@@ -9,13 +9,10 @@ from classes.engine.Printator import Printator
 
 class Apitator:
 
-    onlineApi = "http://5.196.72.181:8080/"
-    localApi = "http://api:8080/"
-    env = ''
-    ip = ''
-    token = ''
-
-    def init(self):
+    def __init__(self):
+        self.token = ''
+        self.onlineApi = "http://5.196.72.181:8080/"
+        self.localApi = "http://api:8080/"
         Printator.success('Checking API')
         try:
             r = requests.post(self.onlineApi + 'ping')
@@ -23,9 +20,7 @@ class Apitator:
                 self.env = 'online'
                 self.ip = self.onlineApi
                 Printator.success(Fore.GREEN + 'API online status ok')
-                return True
         except Exception as e:
-            time.sleep(0.5)
             Printator.success(Fore.RED + 'API online not responding')
             time.sleep(0.5)
             try:
@@ -34,17 +29,12 @@ class Apitator:
                     self.env = 'local'
                     self.ip = self.localApi
                     Printator.success(Fore.GREEN + 'API local status ok')
-                    time.sleep(0.5)
-                    return True
             except Exception as e:
                 Printator.success(Fore.RED + "API local not responding")
                 time.sleep(0.5)
                 Printator.success('To start the local API refer you to https://github.com/Crash-Zeus/pyrpgApi')
-                time.sleep(0.5)
                 Printator.success('If your local API is started and the problem persists checked ip of API container')
-                time.sleep(0.5)
-                Printator.success('The default ip for local API in the program is ' + Fore.GREEN + self.localApi + Fore.RESET)
-                time.sleep(0.5)
+                Printator.success('The default ip for local API in the program is ' + Fore.GREEN + self.localApi)
                 sys.exit()
     
     def login(self, log = False):
@@ -59,9 +49,9 @@ class Apitator:
                 Printator.success('Create an account')
             email = input('Email > ')
             username = input('Pseudo > ')
-            passw = self.registeryPass(self)
+            passw = self.registeryPass()
             if passw == False:
-                self.login(self, 1)
+                self.login(1)
             data = {
                 "username" : username,
                 "email" : email,
@@ -70,13 +60,13 @@ class Apitator:
             r = requests.post(self.ip + 'signup', data)
             if r.status_code == 200:
                 Printator.success(Fore.GREEN + 'Your account have successfully created')
-                self.login(self, 2)
+                self.login(2)
             elif r.status_code == 401:
                 Printator.success(Fore.RED + str(r.json()['message']))
-                self.login(self, 1)
+                self.login(1)
             else:
                 Printator.success(Fore.RED + str(r.json()['message']))
-                self.login(self, True)
+                self.login(True)
         elif logs == 'signup':
             Printator.success('Connect to your account')
             email = input('Email > ')
@@ -92,7 +82,7 @@ class Apitator:
             else:
                 sys.exit(Fore.RED + str(r.json()['message']) + Fore.RESET)
         else:
-            self.login(self)
+            self.login()
 
     def registeryPass(self):
         password = getpass.getpass(prompt='Password > ', stream=None)
@@ -136,7 +126,7 @@ class Apitator:
             return r.json()['message']
 
     def getClass(self, cID = False):
-        apiClass = self.getApiClass(self)
+        apiClass = self.getApiClass()
         if cID == False:
             write = False
             with open('/app/settings/tmp.json', 'a') as f:
